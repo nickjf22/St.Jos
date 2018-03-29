c5436f7b5937f83c9e7065c7cf0de2e2fa1d6507
Nicholas Johnson <nickjf22@gmail.com>
2018-03-29 18:10:53 -0400

    Nearly Completed Project

---
+++ js/app.js
@@ -0,0 +1,52 @@
+/*Everything below here was copied from the JS written in-line*/
+<script language="JavaScript">< !--
+                                        MSFPhover =
+                                    (((navigator.appName == "Netscape") &&
+                                        (parseInt(navigator.appVersion) >= 3)) ||
+                                        ((navigator.appName == "Microsoft Internet Explorer") &&
+                                            (parseInt(navigator.appVersion) >= 4)));
+                                    function MSFPpreload(img) {
+                                        var a = new Image(); a.src = img; return a;
+                                    }
+    // --></script>
+    <script language="JavaScript">< !--
+    if (MSFPhover) {MSFPnav1n = MSFPpreload("_derived/home_cmp_eclipse010_hbtn_p.gif"); MSFPnav1h = MSFPpreload("_derived/home_cmp_eclipse010_hbtn_a.gif"); }
+    // --></script>
+    <a href="./" language="JavaScript" onmouseover="if(MSFPhover) document['MSFPnav1'].src=MSFPnav1h.src"
+        onmouseout="if(MSFPhover) document['MSFPnav1'].src=MSFPnav1n.src">
+        <img src="_derived/home_cmp_eclipse010_hbtn_p.gif" width="140" height="34" border="0" alt="Home"
+            align="middle" name="MSFPnav1">
+                                </a>
+        <script language="JavaScript">< !--
+    if (MSFPhover) {MSFPnav2n = MSFPpreload("_derived/calendar.html_cmp_eclipse010_hbtn.gif"); MSFPnav2h = MSFPpreload("_derived/calendar.html_cmp_eclipse010_hbtn_a.gif"); }
+    // --></script>
+        <a href="calendar.html" language="JavaScript" onmouseover="if(MSFPhover) document['MSFPnav2'].src=MSFPnav2h.src"
+            onmouseout="if(MSFPhover) document['MSFPnav2'].src=MSFPnav2n.src">
+            <img src="_derived/calendar.html_cmp_eclipse010_hbtn.gif" width="140" height="34" border="0" alt="Calendar"
+                align="middle" name="MSFPnav2">
+                <!-- make this link to an actual Google Calendar, not just a text list-->
+                                </a>
+            <script language="JavaScript">< !--
+    if (MSFPhover) {MSFPnav3n = MSFPpreload("_derived/history.html_cmp_eclipse010_hbtn.gif"); MSFPnav3h = MSFPpreload("_derived/history.html_cmp_eclipse010_hbtn_a.gif"); }
+    // --></script>
+            <a href="history.html" language="JavaScript" onmouseover="if(MSFPhover) document['MSFPnav3'].src=MSFPnav3h.src"
+                onmouseout="if(MSFPhover) document['MSFPnav3'].src=MSFPnav3n.src">
+                <img src="_derived/history.html_cmp_eclipse010_hbtn.gif" width="140" height="34" border="0" alt="History"
+                    align="middle" name="MSFPnav3">
+                                </a>
+                <script language="JavaScript">< !--
+    if (MSFPhover) {MSFPnav4n = MSFPpreload("_derived/photos.html_cmp_eclipse010_hbtn.gif"); MSFPnav4h = MSFPpreload("_derived/photos.html_cmp_eclipse010_hbtn_a.gif"); }
+    // --></script>
+                <a href="photos.html" language="JavaScript" onmouseover="if(MSFPhover) document['MSFPnav4'].src=MSFPnav4h.src"
+                    onmouseout="if(MSFPhover) document['MSFPnav4'].src=MSFPnav4n.src">
+                    <img src="_derived/photos.html_cmp_eclipse010_hbtn.gif" width="140" height="34" border="0" alt="Photos"
+                        align="middle" name="MSFPnav4">
+                                </a>
+                    <script language="JavaScript">< !--
+    if (MSFPhover) {MSFPnav5n = MSFPpreload("_derived/articles.html_cmp_eclipse010_hbtn.gif"); MSFPnav5h = MSFPpreload("_derived/articles.html_cmp_eclipse010_hbtn_a.gif"); }
+    // --></script>
+                    <a href="articles.html" language="JavaScript" onmouseover="if(MSFPhover) document['MSFPnav5'].src=MSFPnav5h.src"
+                        onmouseout="if(MSFPhover) document['MSFPnav5'].src=MSFPnav5n.src">
+                        <img src="_derived/articles.html_cmp_eclipse010_hbtn.gif" width="140" height="34" border="0" alt="Articles"
+                            align="middle" name="MSFPnav5">
+                                </a>
---
+++ js/js.js
@@ -0,0 +1,79 @@
+//start slideshow script//
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
+    //end slideshow script//
+
+
+//making side nav hide//
+
+function hide() {
+    var x = document.getElementById("hideSN");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+//  /making side nav hide//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const donateButton = document.getElementById('donateButton');
+
+// donateButton.addEventListener('click', () => {
+//     //put some JS action in here for the donate button to do when clicked//
+// });
+
+// // document.querySelector('.side-nav').style.display = 'none';
+// $('#SN').click(function (hide) {
+
+// });
+
+// var pages = [
+//     'Home',
+//     'About',
+//     'Calendar',
+//     'History',
+//     'Photos',
+//     'Articles',
+//     'Contact'
+// ];
+
+// function print(message) {
+//     document.write(message);
+// }
+
+// function printList(list) {
+//     var listHTML = '<ol>';
+//     for (var i = 0; i < list.length; i += 1) {
+//         listHTML += '<li>' + list[i] + '</li>';
+//     }
+//     print(listHTML);
+// }
+// printList(pages);
+
+/* put this in the HTML <div id="output"> then put this in the JS  var outputDiv = document.getElementById('output'); to reference that specific place in the HTML*/
